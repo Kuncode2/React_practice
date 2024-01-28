d90558e0c318071d24b9703eddb1f21481b78bac
kunal <kuncode2@gmail.com>
2024-01-28 13:15:11 +0530

    lec2

---
+++ lec2.js
@@ -0,0 +1,14 @@
+//arrow function
+function x(){
+    const a =10
+}            
+ // memory heap
+ // data structure for
+ // storing the variable is the call stack
+var x = function(){
+    console.log("This is the animouse function");
+}
+
+var x=()=>{
+// it's take this of the laxical parents // the arrow is also called fat arrow
+}
